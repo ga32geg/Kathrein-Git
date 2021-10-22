787605a7db5b902e6405e2524fd238250d50ecc0
ga32geg <92869610+ga32geg@users.noreply.github.com>
2021-10-22 18:54:22 +0200

    Plotten 3D fertig

--- GUI_Test_reader.py
+++ GUI_Test_reader.py
@@ -79,52 +79,17 @@ class GUI(QtWidgets.QMainWindow, gui3.Ui_MainWindow):
 
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d')
-
-        x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-        y = [5, 6, 2, 3, 13, 4, 1, 2, 4, 8]
-        z = [2, 3, 3, 3, 5, 7, 9, 11, 9, 10]
-
-        ax.scatter(r, y, v, c='r', marker='o')
-
+        surf = ax.scatter(r, y, v, c='r', marker='o')
         ax.set_xlabel('X Label')
         ax.set_ylabel('Y Label')
         ax.set_zlabel('Z Label')
 
-
-        #ax.scatter(2, 3, 4)  # plot the point (2,3,4) on the figure
-
-        # Make data.
-        #X = np.arange(-5, 5, 0.25)
-        #Y = np.arange(-5, 5, 0.25)
         X = r
         Y = y
         X, Y = np.meshgrid(X, Y)
-        #ax.scatter(2, 3, 4)  # plot the point (2,3,4) on the figure
-        R = np.sqrt(X ** 2 + Y ** 2)
-        Z = np.sin(R)
 
-        # Plot the surface.
-        #surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm, linewidth=0, antialiased=False)
-        #ax.scatter(X, Y, v, c='r', marker='o')
+        fig.colorbar(surf, shrink=0.5, aspect=5)
         plt.show()
-        '''
-        # Customize the z axis.
-        ax.set_zlim(-1.01, 1.01)
-        ax.zaxis.set_major_locator(LinearLocator(10))
-        # A StrMethodFormatter is used automatically
-        ax.zaxis.set_major_formatter('{x:.02f}')
-
-        # Add a color bar which maps values to colors.
-       # fig.colorbar(surf, shrink=0.5, aspect=5)   '''
-
-
-    def simple(self):
-        fig = plt.figure(figsize=(4, 4))
-        ax = fig.add_subplot(111, projection='3d')
-        ax.scatter(2, 3, 4)  # plot the point (2,3,4) on the figure
-        plt.show()
-
-
 
 
     def clear(self):
